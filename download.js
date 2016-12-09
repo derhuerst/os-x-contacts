98a7f1a1810098ccea817002259be5e18439c656
Jannis R <mail@jannisr.de>
2016-12-09 14:59:51 +0100

    improve CI build
    Merged from test-ci branch.

--- download.js
+++ download.js
@@ -6,7 +6,11 @@ const path = require('path')
 
 const dest = path.join(__dirname, process.argv[2])
 
-got('https://api.github.com/repos/pepebecker/contacts-cli/releases/4811892', {json: true})
+const headers = process.env.GITHUB_TOKEN ? {
+	Authorization: 'token ' + process.env.GITHUB_TOKEN
+} : {}
+
+got('https://api.github.com/repos/pepebecker/contacts-cli/releases/4811892', {json: true, headers})
 .then((res) => res.body.assets.find((asset) => asset.name === 'contacts-cli').browser_download_url)
 .then((url) => new Promise((yay, nay) => {
 	got.stream(url)
