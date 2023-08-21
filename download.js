6d8a2e5ccd17fcf770dbe0039bb3ff71e9dc5b85
Jannis R <mail@jannisr.de>
2023-08-21 14:42:41 +0200

    Node 18+ 💥💚

--- download.js
+++ download.js
@@ -1,8 +1,7 @@
 'use strict'
 
-const {get} = require('https')
-const {parse: parseUrl} = require('url')
 const {ok} = require('assert')
+const {Writable} = require('node:stream')
 const {createWriteStream} = require('fs')
 const {join: pathJoin} = require('path')
 const pkg = require('./package.json')
@@ -13,97 +12,50 @@ https://api.github.com/repos/pepebecker/contacts-cli/releases/4811892`
 
 const dest = pathJoin(__dirname, process.argv[2])
 
-const headers = process.env.GITHUB_TOKEN ? {
-	Authorization: 'token ' + process.env.GITHUB_TOKEN
-} : {}
-
-const pGet = (...args) => {
-	return new Promise((resolve, reject) => {
-		get(...args, resolve)
-		.once('error', reject)
-	})
+const headers = {
+	'User-Agent': USER_AGENT,
+	...(process.env.GITHUB_TOKEN ? {
+		Authorization: 'token ' + process.env.GITHUB_TOKEN,
+	} : {}),
 }
 
-const readJsonBody = (res) => {
-	return new Promise((resolve, reject) => {
-		res.once('error', reject)
-		let body = ''
-		res.on('data', (chunk) => body += chunk)
-		res.once('end', () => {
-			try {
-				resolve(JSON.parse(body))
-			} catch (err) {
-				reject(err)
-			}
-		})
+;(async () => {
+	const res = await fetch(RELEASE_URL, {
+		redirect: 'follow',
+		headers: {
+			...headers,
+			'Accept': 'application/json',
+		},
 	})
-}
+	if (!res.ok) {
+		const err = new Error(`${res.url}: ${res.status} ${res.statusText}`)
+		err.res = res
+		throw err
+	}
 
-const poorMansFetch = (url, opt = {}, nrOfRedirects = 0) => {
-	ok(nrOfRedirects < 10, 'too many redirects')
-
-	const _url = parseUrl(url)
-	return pGet({
-		protocol: _url.protocol,
-		hostname: _url.hostname,
-		port: _url.port,
-		path: _url.path,
-		headers: Object.assign({
-			'User-Agent': USER_AGENT,
-		}, opt.headers || {}),
-	})
-	.then((res) => {
-		if (res.statusCode === 301 || res.statusCode === 302) {
-			const newUrl = res.headers['location']
-			return poorMansFetch(newUrl, opt, nrOfRedirects + 1)
-		}
-		if (res.statusCode !== 200) {
-			const err = new Error(`request to ${res.url} failed: ${res.statusCode} ${res.statusMessage}`)
-			err.res = res
-			throw err
-		}
-
-		return res
-	})
-}
-
-const poorMansPipeline = (src, dest) => {
-	return new Promise((resolve, reject) => {
-		src
-		.once('error', reject)
-		.pipe(dest)
-		.once('error', reject)
-		.once('finish', () => resolve())
-	})
-}
-
-poorMansFetch(RELEASE_URL, {
-	headers: {
-		'Accept': 'application/json',
-	},
-})
-.then((res) => {
-	const cType = res.headers['content-type']
+	const cType = res.headers.get('content-type')
 	if (cType !== 'application/json; charset=utf-8') {
 		const err = new Error(`${res.url} responded with an invalid content-type: ${cType}`)
 		err.res = res
 		throw err
 	}
 
-	res.setEncoding('utf8')
-	return readJsonBody(res)
-})
-.then((body) => {
-	const asset = body.assets.find((asset) => asset.name === 'contacts-cli')
-	ok(asset, 'contacts-cli asset not found')
-	return poorMansFetch(asset.browser_download_url)
-})
-.then((res) => {
-	return poorMansPipeline(
-		res,
-		createWriteStream(dest)
-	)
-})
+	const assetsBody = await res.json()
+	const asset = assetsBody.assets.find((asset) => asset.name === 'contacts-cli')
+		ok(asset, 'contacts-cli asset not found')
+
+	const assetRes = await fetch(asset.browser_download_url, {
+		redirect: 'follow',
+		headers,
+	})
+	if (!assetRes.ok) {
+		const err = new Error(`${assetRes.url}: ${assetRes.status} ${assetRes.statusText}`)
+		err.res = assetRes
+		throw err
+	}
+
+	await assetRes.body.pipeTo(Writable.toWeb(createWriteStream(dest)))
+})()
 .catch((err) => {
 	console.error(err)
 	process.exit(1)
